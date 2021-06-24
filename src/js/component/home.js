db5605645d884ffd29876dfa19972e3d8dcaaa55
marcoescmont <marcoescmont@gmail.com>
2021-06-24 05:37:18 +0000

    cards done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,17 +1,21 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function NavBar() {
+
+//NavBar//
+
+export const NavBar = props => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<a className="navbar-brand" href="#">
-				Navbar
+		<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+			<a className="navbar-brand text-light" href={props.LPUrl}>
+				<h1>
+					<h1 className={props.LPlogo}></h1> {props.LPname}
+				</h1>
 			</a>
 			<button
-				className="navbar-toggler"
+				className="navbar-toggler float-right"
 				type="button"
 				data-toggle="collapse"
 				data-target="#navbarSupportedContent"
@@ -20,46 +24,122 @@ export function NavBar() {
 				aria-label="Toggle navigation">
 				<span className="navbar-toggler-icon"></span>
 			</button>
-
-			<div
-				className="collapse navbar-collapse"
-				id="navbarSupportedContent">
-				<ul className="navbar-nav mr-auto">
-					<li className="nav-item dropdown">
-						<a
-							className="nav-link dropdown-toggle"
-							href="#"
-							id="navbarDropdown"
-							role="button"
-							data-toggle="dropdown"
-							aria-haspopup="true"
-							aria-expanded="false">
-							Dropdown
-						</a>
-						<div
-							className="dropdown-menu"
-							aria-labelledby="navbarDropdown">
-							<a className="dropdown-item" href="#">
-								Action
-							</a>
-							<a className="dropdown-item" href="#">
-								Another action
-							</a>
-							<div className="dropdown-divider"></div>
-							<a className="dropdown-item" href="#">
-								Something else here
-							</a>
-						</div>
-					</li>
-				</ul>
+			<div className="btn-group ml-auto">
+				<button
+					type="button"
+					className="btn text-light"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false">
+					<h2 className={props.DropdownIcon}></h2>
+				</button>
+				<div className="dropdown-menu dropdown-menu-right">
+					<a
+						className="dropdown-item"
+						href={props.DropdownOp1URL}
+						type="button">
+						{props.DropdownOp1}
+					</a>
+					<a
+						className="dropdown-item"
+						href={props.DropdownOp2URL}
+						type="button">
+						{props.DropdownOp2}
+					</a>
+					<a
+						className="dropdown-item"
+						href={props.DropdownOp3URL}
+						type="button">
+						{props.DropdownOp3}
+					</a>
+					<a
+						className="dropdown-item"
+						href={props.DropdownOp4URL}
+						type="button">
+						{props.DropdownOp4}
+					</a>
+				</div>
 			</div>
 		</nav>
 	);
-}
+};
 
-export function Jumbotron() {
-	return <div>hola como estas</div>;
-}
+NavBar.propTypes = {
+	LPname: PropTypes.string,
+	DropdownIcon: PropTypes.string,
+	LPlogo: PropTypes.string,
+	LPUrl: PropTypes.string,
+	DropdownOp1: PropTypes.string,
+	DropdownOp2: PropTypes.string,
+	DropdownOp3: PropTypes.string,
+	DropdownOp4: PropTypes.string,
+	DropdownOp1URL: PropTypes.string,
+	DropdownOp2URL: PropTypes.string,
+	DropdownOp3URL: PropTypes.string,
+	DropdownOp4URL: PropTypes.string
+};
 
-for (let a = 0; a <= 3; a++);
-export function Cards() {}
+//  Jumbotron  //
+
+export const Jumbotron = props => {
+	return (
+		<div className="jumbotron">
+			<h1 className="display-4">{props.Title}</h1>
+			<p className="lead">{props.Header}</p>
+			<hr className="my-4" />
+			<p>{props.Content}</p>
+			<a className="btn btn-primary btn-lg" href="#" role="button">
+				{props.ButtonName}
+			</a>
+		</div>
+	);
+};
+
+Jumbotron.propTypes = {
+	Title: PropTypes.string,
+	Header: PropTypes.string,
+	Content: PropTypes.string,
+	ButtonName: PropTypes.string
+};
+
+//  Cards  //
+
+export const Cards = props => {
+	return (
+		<div className="card">
+			<img
+				src={props.ImageURL}
+				className="card-img-top"
+				alt={props.ImageAlt}
+			/>
+			<div className="card-body">
+				<h5 className="card-title">{props.Title}</h5>
+				<p className="card-text">{props.Content}</p>
+			</div>
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item">{props.ExtraContent1}</li>
+				<li className="list-group-item">{props.ExtraContent2}</li>
+				<li className="list-group-item">{props.ExtraContent3}</li>
+			</ul>
+			<div className="card-footer text-center">
+				<a
+					href="#"
+					type="button"
+					className="card-link btn btn-secondary">
+					{props.ButtonContent}
+				</a>
+			</div>
+		</div>
+	);
+};
+
+Cards.propTypes = {
+	ImageURL: PropTypes.string,
+	ImageAlt: PropTypes.string,
+	Title: PropTypes.string,
+	Content: PropTypes.string,
+	ExtraContent1: PropTypes.string,
+	ExtraContent2: PropTypes.string,
+	ExtraContent3: PropTypes.string,
+	ButtonContent: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,12 +12,82 @@ import "../styles/index.scss";
 import { NavBar } from "./component/home.js";
 import { Jumbotron } from "./component/home.js";
 import { Cards } from "./component/home.js";
+
 //render your react application
 ReactDOM.render(
 	<div>
-		<NavBar />
-		<Jumbotron />
-		<Cards />
+		<div>
+			<NavBar
+				LPname="Landing Page"
+				DropdownIcon="far fa-caret-square-down"
+				LPlogo="fab fa-react"
+				LPUrl="#"
+				DropdownOp1="Home"
+				DropdownOp2="About"
+				DropdownOp3="Services"
+				DropdownOp4="Contact"
+				DropdownOp1URL="#"
+				DropdownOp2URL="#"
+				DropdownOp3URL="#"
+				DropdownOp4URL="#"
+			/>
+		</div>
+		<div className="w-75 m-auto">
+			<div>
+				<Jumbotron
+					Title="Hello, world!"
+					Header="This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
+					Content="It uses utility classNamees for typography and spacing to space content out within the larger container."
+					ButtonName="Learn more"
+				/>
+			</div>
+			<div className="card-deck">
+				<Cards
+					ImageURL=""
+					ImageAlt=""
+					Title="THIS"
+					Content="Some quick example text to build on the card
+					title and make up the bulk of the cards content."
+					ExtraContent1="You"
+					ExtraContent2="Can"
+					ExtraContent3="Put"
+					ButtonContent="The sky"
+				/>
+				<Cards
+					ImageURL=""
+					ImageAlt=""
+					Title="IS"
+					Content="Some quick example text to build on the card
+					title and make up the bulk of the cards content."
+					ExtraContent1="Whatever"
+					ExtraContent2="You"
+					ExtraContent3="Want"
+					ButtonContent="Is"
+				/>
+				<Cards
+					ImageURL=""
+					ImageAlt=""
+					Title="REALLY"
+					Content="Some quick example text to build on the card
+					title and make up the bulk of the cards content."
+					ExtraContent1="In"
+					ExtraContent2="This"
+					ExtraContent3="Ones"
+					ButtonContent="The"
+				/>
+				<Cards
+					ImageURL=""
+					ImageAlt=""
+					Title="AMAZING!!"
+					Content="Some quick example text to build on the card
+					title and make up the bulk of the cards content."
+					ExtraContent1="Is"
+					ExtraContent2="Really"
+					ExtraContent3="Cool"
+					ButtonContent="Limit"
+				/>
+			</div>
+		</div>
 	</div>,
 	document.querySelector("#app")
 );
